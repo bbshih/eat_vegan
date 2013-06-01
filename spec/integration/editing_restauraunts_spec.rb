3c4499175e2b5324ca7ebef21be426370d21d7a5
Billy Shih <me@billyshih.com>
2013-05-31 18:01:39 -0700

    Updated readme

---
+++ spec/integration/editing_restauraunts_spec.rb
@@ -0,0 +1,14 @@
+# require 'spec_helper'
+
+# feature "Editing new restaurants" do
+#   scenario "can edit a new restaurant" do
+#     visit "/"
+#     fill_in "Name", with: "Veggie Grill"
+#     fill_in "Address", with: "446 Terry Ave N"
+#     fill_in "City", with: "Seattle"
+#     fill_in "State", with: "WA"
+#     fill_in "Zip code", with: "98109"
+#     click_button "Add new restaurant"
+#     page.should have_content("Restaurant added successfully")
+#   end
+# end
