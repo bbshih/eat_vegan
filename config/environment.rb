def3bb43904cc0bdfbe630941fb7cb78f3c5fe43
Billy Shih <me@billyshih.com>
2013-06-04 14:42:30 -0700

    Setup mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EatVegan::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['yqx92b2o'],
+  :password       => ENV['app16106217@heroku.com'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
